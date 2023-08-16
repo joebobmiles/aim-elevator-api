3f9318ccea3008902ab6b9e0eb71e3ab3bc344ff
Joseph R Miles <joe@jrm.dev>
2023-08-16 15:50:32 -0700

    feat: added endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-import express from 'express'
+import express, { Request } from 'express'
 import 'dotenv/config'
 
 const app = express()
+const api = express.Router()
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
+api.get('/', (req, res) => {
+  res.send([])
 })
+
+api.get('/next', (req, res) => {
+  res.send(null)
+})
+
+api.route('/:floorId')
+  .put((req, res) => {
+    res.send(null)
+  })
+  .delete((req, res) => {
+    res.send(null)
+  })
+
+app.use('/api/v1/elevator/:elevatorId/floors', api)
 
 const port = process.env.PORT
 app.listen(port, () => {
